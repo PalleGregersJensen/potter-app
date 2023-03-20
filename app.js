571616583dc47d756a0a7c069b581a3655504882
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-03-20 21:01:36 +0100

    Øvelse 2 virker ikke helt

--- app.js
+++ app.js
@@ -3,30 +3,30 @@
 // ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
 
-function initApp() {
+// function initApp() {
   // Harry Potter
-  const potterName = "Harry Potter";
-  const potterGender = "male";
-  const potterHouse = "Gryffindor";
-  const potterDateOfBirth = "31-07-1980";
-  const potterAncestry = "half-blood";
-  const potterEyeColour = "green";
-  const potterHairColour = "black";
-  const potterActor = "Daniel Radcliffe";
-  const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+  // const potterName = "Harry Potter";
+  // const potterGender = "male";
+  // const potterHouse = "Gryffindor";
+  // const potterDateOfBirth = "31-07-1980";
+  // const potterAncestry = "half-blood";
+  // const potterEyeColour = "green";
+  // const potterHairColour = "black";
+  // const potterActor = "Daniel Radcliffe";
+  // const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 
   // Hermione Granger
-  const hermioneName = "Hermione Granger";
-  const hermioneGender = "female";
-  const hermioneHouse = "Gryffindor";
-  const hermioneDateOfBirth = "17-12-1900";
-  const hermioneAncestry = "half-blood";
-  const hermioneEyeColour = "green";
-  const hermioneHairColour = "black";
-  const hermioneActor = "Daniel Radcliffe";
-  const hermioneImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+  // const hermioneName = "Hermione Granger";
+  // const hermioneGender = "female";
+  // const hermioneHouse = "Gryffindor";
+  // const hermioneDateOfBirth = "17-12-1900";
+  // const hermioneAncestry = "half-blood";
+  // const hermioneEyeColour = "green";
+  // const hermioneHairColour = "black";
+  // const hermioneActor = "Daniel Radcliffe";
+  // const hermioneImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 
-  // Ron Weasley
+  //  Ron Weasley
   // const ronName = "Ron Wreasley";
   // const ronGender = "male";
   // const ronHouse = "Gryffindor";
@@ -37,82 +37,100 @@ function initApp() {
   // const ronActor = "Daniel Radcliffe";
   // const ronImage = "http://hp-api.herokuapp.com/images/harry.jpg";
 
-  showCharacter(
-    potterImage,
-    potterName,
-    potterHouse,
-    potterDateOfBirth,
-    potterActor
-  );
+  // showCharacter(
+    // potterImage,
+    // potterName,
+    // potterHouse,
+    // potterDateOfBirth,
+    // potterActor
+  // );
 
-  showCharacter(
-    hermioneImage,
-    hermioneName,
-    hermioneHouse,
-    hermioneDateOfBirth,
-    hermioneActor
-  );
+  // showCharacter(
+    // hermioneImage,
+    // hermioneName,
+    // hermioneHouse,
+    // hermioneDateOfBirth,
+    // hermioneActor
+  // );
 
-  showCharacter(
-    "http://hp-api.herokuapp.com/images/ron.jpg",
-    "Ron Weasley",
-    "Gryffindor",
-    "01-03-1980",
-    "Rupert Grint"
-  );
+  // showCharacter(
+    // "http://hp-api.herokuapp.com/images/ron.jpg",
+    // "Ron Weasley",
+    // "Gryffindor",
+    // "01-03-1980",
+    // "Rupert Grint"
+  // );
 
-  console.log(
-    potterName,
-    potterGender,
-    potterHouse,
-    potterDateOfBirth,
-    potterAncestry,
-    potterEyeColour,
-    potterHairColour,
-    potterActor,
-    potterImage
-  );
-}
+  // console.log(
+    // potterName,
+    // potterGender,
+    // potterHouse,
+    // potterDateOfBirth,
+    // potterAncestry,
+    // potterEyeColour,
+    // potterHairColour,
+    // potterActor,
+    // potterImage
+  // );
+// }
 
-function showCharacter(image, name, house, dateOfBirth, actor) {
-  console.log(image);
-  console.log(name);
-  console.log(house);
-  console.log(dateOfBirth);
-  console.log(actor);
+// function showCharacter(image, name, house, dateOfBirth, actor) {
+  // console.log(image);
+  // console.log(name);
+  // console.log(house);
+  // console.log(dateOfBirth);
+  // console.log(actor);
 
-  const articleElement = document.createElement("article");
-  const imageElement = document.createElement("img");
-  const nameElement = document.createElement("h2");
-  const houseElement = document.createElement("p");
-  const dateOfBirthElement = document.createElement("p");
-  const actorElement = document.createElement("p");
+  // const articleElement = document.createElement("article");
+  // const imageElement = document.createElement("img");
+  // const nameElement = document.createElement("h2");
+  // const houseElement = document.createElement("p");
+  // const dateOfBirthElement = document.createElement("p");
+  // const actorElement = document.createElement("p");
 
-  articleElement.append(
-    imageElement,
-    nameElement,
-    houseElement,
-    dateOfBirthElement,
-    actorElement
-  );
+  // articleElement.append(
+    // imageElement,
+    // nameElement,
+    // houseElement,
+    // dateOfBirthElement,
+    // actorElement
+  // );
 
-  imageElement.src = image;
-  nameElement.textContent = name;
-  houseElement.textContent = house;
-  dateOfBirthElement.textContent = dateOfBirth;
-  actorElement.textContent = actor;
+  // imageElement.src = image;
+  // nameElement.textContent = name;
+  // houseElement.textContent = house;
+  // dateOfBirthElement.textContent = dateOfBirth;
+  // actorElement.textContent = actor;
 
-  document.querySelector("#characters").appendChild(articleElement);
+  // document.querySelector("#characters").appendChild(articleElement);
 
-  console.log(imageElement);
-  console.log(nameElement);
-  console.log(dateOfBirthElement);
-  console.log(houseElement);
-  console.log(actorElement);
-}
+  // console.log(imageElement);
+  // console.log(nameElement);
+  // console.log(dateOfBirthElement);
+  // console.log(houseElement);
+  // console.log(actorElement);
+// }
 
-function showCharacter() {
-  <article>
-    
-  </article>
-}
+// function showCharacter(image, name, house, dateOfBirth, actor) {
+// const html =
+/*html*/
+// `<article>
+// (<img src =${image}>)
+// <h2>${name}</h2>
+// <p>${house}</p>
+// <p>${dateOfBirth}</p>
+// <p>${actor}</p>
+// </article>`;
+
+// document.querySelector("#characters").insertAdjacentHTML("beforeend", html);
+// list.querySelector("li:last-child button").addEventListener("click", removeToDo);
+// }
+
+// showCharacter(
+// console.log("Er vi her?"),
+// potterImage,
+// potterName,
+// potterHouse,
+// potterDateOfBirth,
+// potterActor
+// );
